3076e56f5b313840e1673b941f995182509b9e96
Ruben valencia <rd_navarrete@outlook.com>
2023-05-28 11:11:57 -0500

    rutas preparadas

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { RecetasComponent } from './components/recetas/recetas.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { SesionComponent } from './components/sesion/sesion.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, RecetasComponent, InicioComponent, AcercaComponent, SesionComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
