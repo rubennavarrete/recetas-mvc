7cba1d7d3a657a2c92d9a1559ffb501e283ae6d2
DanielConduri <jonathand.tene@espoch.edu.ec>
2023-05-28 13:22:56 -0500

    cambios sn el server ruta de prueba

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -1,8 +1,8 @@
 import Sequelize from "sequelize";
 
 // conexion a la base de datos
-export const sequelize = new Sequelize("recetasdb", "postgres", "3919", {
+export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
   host: "localhost",
   dialect: "postgres",
-  port: "5454"
+  port: "5432"
 });
