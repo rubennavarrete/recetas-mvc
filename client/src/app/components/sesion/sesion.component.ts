8fd01c240ce14b3d67586004f7e0a063a76cb2a4
Ruben valencia <rd_navarrete@outlook.com>
2023-05-28 23:10:07 -0500

    cambio de color del blur

--- client/src/app/components/sesion/sesion.component.ts
+++ client/src/app/components/sesion/sesion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
+import { SesionService } from 'src/app/core/service/sesion.service';
 
 @Component({
   selector: 'app-sesion',
@@ -10,8 +11,10 @@ import { Subject, takeUntil } from 'rxjs';
 export class SesionComponent implements OnInit, OnDestroy {
   myForm!: FormGroup;
   private destroy$ = new Subject<any>();
+  claveInc: boolean = true;
+  message: string = '';
 
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder, public srvSesion: SesionService) {
     this.myForm = this.fb.group({
       usuario: ['', [Validators.required]],
       password: ['', [Validators.required]],
@@ -22,6 +25,21 @@ export class SesionComponent implements OnInit, OnDestroy {
 
   IniciarSesion() {
     console.log(this.myForm.value);
+    this.srvSesion
+      .valSesion(this.myForm.value)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (data: any) => {
+          console.log(data.status);
+          this.srvSesion.sesion = data.status;
+          this.claveInc = data.status;
+          this.message = data.message;
+
+          if (this.srvSesion.sesion == true) {
+            window.location.href = '/recetas';
+          }
+        },
+      });
   }
 
   ngOnDestroy(): void {
--- client/src/app/core/service/sesion.service.ts
+++ client/src/app/core/service/sesion.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import config from 'config/config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SesionService {
+  private urlSesion: string = config.URL_API_BASE + 'usuario';
 
-  constructor() { }
+  sesion!: boolean;
+
+  constructor(private http: HttpClient) {}
+
+  valSesion(data: any) {
+    return this.http.post<any>(
+      this.urlSesion,
+      { data },
+      { withCredentials: true }
+    );
+  }
 }
