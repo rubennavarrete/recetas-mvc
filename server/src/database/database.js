f7ebc4e290b0070ae9c82cae25537cf41d7430b5
Kevin <angelo,espinoza@espoch,edu,ec>
2023-06-06 11:44:08 -0500

    trayendo cambios nuevamente

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,11 +2,8 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-<<<<<<< HEAD
-export const sequelize = new Sequelize("recetasdb", "postgres", "Holamundo14", {
-=======
-export const sequelize = new Sequelize("recetasdb", "postgres", "123456", {
->>>>>>> e9b22f5f0ca04a3a63374f8fc50d95958f1b4f43
-  host: "localhost",
+export const sequelize = new Sequelize("recetasdb", "postgres", "3919", {
+  host: "127.0.0.1",
   dialect: "postgres",
+  port: "5454"
 });
